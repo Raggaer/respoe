15edca707966a93401193632509afe997e3790aa
Raggaer <nakotoffana@gmail.com>
2017-12-02 03:15:06 +0100

    Client - send private messages

---
+++ client/send_private_message_test.go
@@ -0,0 +1,21 @@
+package client
+
+import (
+	"os"
+	"testing"
+)
+
+func TestSendPrivateMessage(t *testing.T) {
+	c, err := New()
+	if err != nil {
+		t.Fatalf("Unable to create http client: %v", err)
+	}
+
+	if err := c.Login(os.Getenv("RESPOE_EMAIL"), os.Getenv("RESPOE_PASSWORD")); err != nil {
+		t.Fatalf("Unable to login: %v", err)
+	}
+
+	if err := c.SendPrivateMessage([]string{"Punckanuto"}, "Hello from Respoe", "Hi brother"); err != nil {
+		t.Fatalf("Unable to send private message: %v", err)
+	}
+}
